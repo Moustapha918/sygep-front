efaa9bd34faaa4d8122c1ba8469c566628b9a640
moustapha <kabermoustapha@gmail.com>
2018-07-05 08:36:52 +0200

    clientService

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {UserData} from "../pages/tables/filtering-table/filtering-table.component";
+import {environment} from "../../environments/environment";
+
+@Injectable()
+export class ClientService {
+
+  constructor(private http : HttpClient) {
+
+  }
+
+  addUser() : Observable<boolean>{
+    const credentials = {username: "deyd", password: "deyd", email : "kabermoustapha@gmail.com"};
+    let result = this.http.post( "users/add", credentials) ;
+    return result;
+  }
+
+  getClientq(): Observable<UserData> {
+    const clientsList = [];
+    let d1 = {Id: '1',name :'Ahmed', }
+    return Observable.of(null);
+  }
+
+
+}
