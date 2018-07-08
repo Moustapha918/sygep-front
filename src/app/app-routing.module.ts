1ccc5ba194ee2e9827d8d08621132bf77f76ea4d
moustapha <kabermoustapha@gmail.com>
2018-07-08 19:58:03 +0200

    Netoyage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,31 +7,15 @@ import { ExtraLayoutComponent }         from './layouts/extra/extra.component';
 import { PageDashboardComponent }       from './pages/dashboards/dashboard-1/dashboard.component';
 
 import { PageSignIn1Component }         from './pages/extra-pages/sign-in-1/sign-in-1.component';
-import { PageSignIn2Component }         from './pages/extra-pages/sign-in-2/sign-in-2.component';
-import { PageSignIn3Component }         from './pages/extra-pages/sign-in-3/sign-in-3.component';
-import { PageSignUp1Component }         from './pages/extra-pages/sign-up-1/sign-up-1.component';
-import { PageSignUp2Component }         from './pages/extra-pages/sign-up-2/sign-up-2.component';
-import { PageForgotComponent }          from './pages/extra-pages/forgot/forgot.component';
-import { PageConfirmComponent }         from './pages/extra-pages/confirm/confirm.component';
-import { Page404Component }             from './pages/extra-pages/page-404/page-404.component';
-import { Page500Component }             from './pages/extra-pages/page-500/page-500.component';
 import { PageFilteringTableComponent }  from './pages/tables/filtering-table/filtering-table.component';
 
 const defaultRoutes: Routes = [
   { path: 'dashboard', component: PageDashboardComponent },
-  { path: 'sign-in', component: PageSignIn2Component },
-  { path: 'sign-up', component: PageSignUp2Component },
   { path: 'filtering-table', component: PageFilteringTableComponent },
 ];
 
 const extraRoutes: Routes = [
   { path: 'sign-in', component: PageSignIn1Component },
-  { path: 'sign-in-social', component: PageSignIn3Component },
-  { path: 'sign-up', component: PageSignUp1Component },
-  { path: 'forgot', component: PageForgotComponent },
-  { path: 'confirm', component: PageConfirmComponent },
-  { path: 'page-404', component: Page404Component },
-  { path: 'page-500', component: Page500Component },
 ];
 
 export const routes: Routes = [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,7 +5,6 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NiComponentsModule }              from '../ni-components/ni-components.module';
 
 import {
-  MatAutocompleteModule,
   MatButtonModule,
   MatButtonToggleModule,
   MatCardModule,
@@ -33,36 +32,15 @@ import {
   MatSortModule,
   MatTableModule,
   MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule
+
 } from '@angular/material';
 import { ChartsModule }                    from 'ng2-charts';
 import { NgxChartsModule }                 from '@swimlane/ngx-charts';
 import { CalendarModule }                  from 'angular-calendar';
 import { AgmCoreModule }                   from '@agm/core';
 import { AmChartsModule }                  from "@amcharts/amcharts3-angular";
-
 import { PageDashboardComponent }          from './dashboards/dashboard-1/dashboard.component';
-import { PageDashboard2Component }         from './dashboards/dashboard-2/dashboard-2.component';
-import { PageDashboard3Component }         from './dashboards/dashboard-3/dashboard-3.component';
-import { PageNiFilesComponent }            from './ni-components/file/file.component';
-import { PageNiCardsComponent }            from './ni-components/card/card.component';
-import { PageNiChatComponent }             from './ni-components/chat/chat.component';
-import { PageNiAlertsComponent }           from './ni-components/alert/alert.component';
-import { PageNiBadgesComponent }           from './ni-components/badge/badge.component';
-import { PageNiBreadcrumbsComponent }      from './ni-components/breadcrumb/breadcrumb.component';
-import { PageNiHTimelineComponent }        from './ni-components/h-timeline/h-timeline.component';
-
 import { PageSignIn1Component }            from './extra-pages/sign-in-1/sign-in-1.component';
-import { PageSignIn2Component }            from './extra-pages/sign-in-2/sign-in-2.component';
-import { PageSignIn3Component }            from './extra-pages/sign-in-3/sign-in-3.component';
-import { PageSignUp1Component }            from './extra-pages/sign-up-1/sign-up-1.component';
-import { PageSignUp2Component }            from './extra-pages/sign-up-2/sign-up-2.component';
-import { PageForgotComponent }             from './extra-pages/forgot/forgot.component';
-import { PageConfirmComponent }            from './extra-pages/confirm/confirm.component';
-import { Page404Component }                from './extra-pages/page-404/page-404.component';
-import { Page500Component }                from './extra-pages/page-500/page-500.component';
-import { PageNiButtonsComponent }          from './ni-components/buttons/buttons.component';
 import {PageFilteringTableComponent} from "./tables/filtering-table/filtering-table.component";
 
 @NgModule({
@@ -73,7 +51,6 @@ import {PageFilteringTableComponent} from "./tables/filtering-table/filtering-ta
 
     NiComponentsModule,
 
-    MatAutocompleteModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
@@ -101,8 +78,6 @@ import {PageFilteringTableComponent} from "./tables/filtering-table/filtering-ta
     MatSortModule,
     MatTableModule,
     MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
     ChartsModule,
     NgxChartsModule,
     CalendarModule.forRoot(),
@@ -114,48 +89,12 @@ import {PageFilteringTableComponent} from "./tables/filtering-table/filtering-ta
   declarations: [
     PageFilteringTableComponent,
     PageDashboardComponent,
-    PageDashboard2Component,
-    PageDashboard3Component,
-    PageNiFilesComponent,
-    PageNiCardsComponent,
-    PageNiChatComponent,
-    PageNiAlertsComponent,
-    PageNiBadgesComponent,
-    PageNiBreadcrumbsComponent,
-    PageNiHTimelineComponent,
     PageSignIn1Component,
-    PageSignIn2Component,
-    PageSignIn3Component,
-    PageSignUp1Component,
-    PageSignUp2Component,
-    PageForgotComponent,
-    PageConfirmComponent,
-    Page404Component,
-    Page500Component,
 
-    PageNiButtonsComponent
   ],
   exports: [
     PageDashboardComponent,
-    PageDashboard2Component,
-    PageDashboard3Component,
-    PageNiFilesComponent,
-    PageNiCardsComponent,
-    PageNiChatComponent,
-    PageNiAlertsComponent,
-    PageNiBadgesComponent,
-    PageNiBreadcrumbsComponent,
-    PageNiHTimelineComponent,
     PageSignIn1Component,
-    PageSignIn2Component,
-    PageSignIn3Component,
-    PageSignUp1Component,
-    PageSignUp2Component,
-    PageForgotComponent,
-    PageConfirmComponent,
-    Page404Component,
-    Page500Component,
-    PageNiButtonsComponent
   ],
   entryComponents: [
 
